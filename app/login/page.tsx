97b518b1ab31c4fbec4253bbe8b986f8a50bd31d
Lr.S <78878365+laur5287@users.noreply.github.com>
2024-01-24 14:06:12 +0200

    build bug fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { headers, cookies } from 'next/headers'
-import { createClient } from '@/utils/supabase/server'
+// import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
 export default function Login({
@@ -8,49 +8,50 @@ export default function Login({
 }: {
 	searchParams: { message: string }
 }) {
-	const signIn = async (formData: FormData) => {
-		'use server'
 
-		const email = formData.get('email') as string
-		const password = formData.get('password') as string
-		const cookieStore = cookies()
-		const supabase = createClient(cookieStore)
+	// const signIn = async (formData: FormData) => {
+	// 	'use server'
 
-		const { error } = await supabase.auth.signInWithPassword({
-			email,
-			password,
-		})
+	// 	const email = formData.get('email') as string
+	// 	const password = formData.get('password') as string
+	// 	const cookieStore = cookies()
+	// const supabase = createClient(cookieStore)
 
-		if (error) {
-			return redirect('/login?message=Could not authenticate user')
-		}
+	// const { error } = await supabase.auth.signInWithPassword({
+	// 	email,
+	// 	password,
+	// })
 
-		return redirect('/')
-	}
+	// if (error) {
+	// 	return redirect('/login?message=Could not authenticate user')
+	// }
 
-	const signUp = async (formData: FormData) => {
-		'use server'
+	// 	return redirect('/')
+	// }
 
-		const origin = headers().get('origin')
-		const email = formData.get('email') as string
-		const password = formData.get('password') as string
-		const cookieStore = cookies()
-		const supabase = createClient(cookieStore)
+	// const signUp = async (formData: FormData) => {
+	// 	'use server'
 
-		const { error } = await supabase.auth.signUp({
-			email,
-			password,
-			options: {
-				emailRedirectTo: `${origin}/auth/callback`,
-			},
-		})
+	// 	const origin = headers().get('origin')
+	// 	const email = formData.get('email') as string
+	// 	const password = formData.get('password') as string
+	// 	const cookieStore = cookies()
+	// const supabase = createClient(cookieStore)
 
-		if (error) {
-			return redirect('/login?message=Could not authenticate user')
-		}
+	// 	const { error } = await supabase.auth.signUp({
+	// 		email,
+	// 		password,
+	// 		options: {
+	// 			emailRedirectTo: `${origin}/auth/callback`,
+	// 		},
+	// 	})
 
-		return redirect('/login?message=Check email to continue sign in process')
-	}
+	// 	if (error) {
+	// 		return redirect('/login?message=Could not authenticate user')
+	// 	}
+
+	// 	return redirect('/login?message=Check email to continue sign in process')
+	// }
 
 	return (
 		<div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
@@ -77,7 +78,7 @@ export default function Login({
 
 			<form
 				className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-				action={signIn}
+			// action={signIn}
 			>
 				<label className="text-md" htmlFor="email">
 					Email
@@ -102,7 +103,7 @@ export default function Login({
 					Sign In
 				</button>
 				<button
-					formAction={signUp}
+					// formAction={signUp}
 					className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
 				>
 					Sign Up
